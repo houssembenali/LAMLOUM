31f3eb82aa2e0aceffb823f5dc25e51954d18772
Houssem Ben Ali <houssembenali90@gmail.com>
2022-05-06 14:28:20 +0100

    Update achref.py

--- achref.py
+++ achref.py
@@ -12,26 +12,13 @@ with open('hoos.csv', newline='') as csvfile:
     for row in reader:
         i=i+1
         descriptions=""
-        if "SERVICE" in row['Trigger'].upper():
-            print("***************************")
-            print(i)
-            print(row['host'])
-
-        #print(row['Trigger'])
         strJson = row['Trigger']
         if strJson == 'Trigger':
             continue
         cleanJson = (strJson[:-1])[:1]+'"'+(strJson[:-1])[1:]
         y=json.loads(cleanJson)
-        #print(cleanJson)
-        #print("***********************")
         for row2 in y['result']:
             #print(row2)
             if "SERVICE" in row2['description'].upper():
                 descriptions =descriptions+ ", \""+ str(row2['description'])+ "\""
-            #print("$$$$$$$$$$")
-        #print("***********************")
-        #print(y['result'][1])
-        #z=json.loads(y['triggerid'])
         f.writelines(row['host']+" "+descriptions+"\n")
-
